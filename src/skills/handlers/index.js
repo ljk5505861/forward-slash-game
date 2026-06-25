43812081a52c93fb1c7626082b00dc3b09503cb7
ljk5505861 <17605047106@163.com>
2026-06-25 11:01:42 +0800

    Register the first flame core skill handlers

--- src/skills/handlers/index.js
+++ src/skills/handlers/index.js
@@ -1,5 +1,16 @@
 import { configureEntryArchetypeSkills, EntryFireballSkill, EntrySwordSkill, EntryPoisonNeedleSkill, EntryHeavyHitSkill, EntryIronWallSkill, EntryMovementSkill } from './EntryArchetypeSkills.js';
+import { configureFlameCoreSkills, FlameSpraySkill, BurnBurstSkill } from './FlameCoreSkills.js';
 
 configureEntryArchetypeSkills();
+configureFlameCoreSkills();
 
-export const SKILL_HANDLERS={entry_fireball:EntryFireballSkill,entry_sword:EntrySwordSkill,entry_poison_needle:EntryPoisonNeedleSkill,entry_heavy_hit:EntryHeavyHitSkill,entry_iron_wall:EntryIronWallSkill,entry_movement:EntryMovementSkill};
+export const SKILL_HANDLERS={
+  entry_fireball:EntryFireballSkill,
+  entry_sword:EntrySwordSkill,
+  entry_poison_needle:EntryPoisonNeedleSkill,
+  entry_heavy_hit:EntryHeavyHitSkill,
+  entry_iron_wall:EntryIronWallSkill,
+  entry_movement:EntryMovementSkill,
+  flame_spray:FlameSpraySkill,
+  burn_burst:BurnBurstSkill,
+};
