e8783b4189cf8fc36a1be174673ce73ad518aeec
ljk5505861 <17605047106@163.com>
2026-06-25 11:26:36 +0800

    Register the first defense core skill handlers

--- src/skills/handlers/index.js
+++ src/skills/handlers/index.js
@@ -2,10 +2,12 @@ import { configureEntryArchetypeSkills, EntryFireballSkill, EntrySwordSkill, Ent
 import { configureFlameCoreSkills, FlameSpraySkill, BurnBurstSkill } from './FlameCoreSkills.js';
 import { configureSwordCoreSkills, SplitSwordSkill, RotatingSwordSkill } from './SwordCoreSkills.js';
 import { configureStrengthCoreSkills, GiantForceSkill, BloodthirstSkill } from './StrengthCoreSkills.js';
+import { configureDefenseCoreSkills, ThornArmorSkill, GuardianShieldSkill } from './DefenseCoreSkills.js';
 
 configureEntryArchetypeSkills();
 configureFlameCoreSkills();
 configureSwordCoreSkills();
 configureStrengthCoreSkills();
+configureDefenseCoreSkills();
 
-export const SKILL_HANDLERS={entry_fireball:EntryFireballSkill,entry_sword:EntrySwordSkill,entry_poison_needle:EntryPoisonNeedleSkill,entry_heavy_hit:EntryHeavyHitSkill,entry_iron_wall:EntryIronWallSkill,entry_movement:EntryMovementSkill,flame_spray:FlameSpraySkill,burn_burst:BurnBurstSkill,split_sword:SplitSwordSkill,rotating_sword:RotatingSwordSkill,giant_force:GiantForceSkill,bloodthirst:BloodthirstSkill};
+export const SKILL_HANDLERS={entry_fireball:EntryFireballSkill,entry_sword:EntrySwordSkill,entry_poison_needle:EntryPoisonNeedleSkill,entry_heavy_hit:EntryHeavyHitSkill,entry_iron_wall:EntryIronWallSkill,entry_movement:EntryMovementSkill,flame_spray:FlameSpraySkill,burn_burst:BurnBurstSkill,split_sword:SplitSwordSkill,rotating_sword:RotatingSwordSkill,giant_force:GiantForceSkill,bloodthirst:BloodthirstSkill,thorn_armor:ThornArmorSkill,guardian_shield:GuardianShieldSkill};
