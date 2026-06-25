549fa66ee9c663294e105fff4171242adff8476f
ljk5505861 <17605047106@163.com>
2026-06-25 09:46:21 +0800

    Register archetype entry skill handlers

--- src/skills/handlers/index.js
+++ src/skills/handlers/index.js
@@ -6,4 +6,8 @@ import HangingBladeSkill from './HangingBladeSkill.js';
 import MirrorMarchSkill from './MirrorMarchSkill.js';
 import JudgmentPendulumSkill from './JudgmentPendulumSkill.js';
 import TimeLoanSkill from './TimeLoanSkill.js';
-export const SKILL_HANDLERS={bone_boomerang:BoneBoomerangSkill,shadow_fist:ShadowFistSkill,bullet_eater:BulletEaterSkill,parasite_lantern:ParasiteLanternSkill,hanging_blade:HangingBladeSkill,mirror_march:MirrorMarchSkill,judgment_pendulum:JudgmentPendulumSkill,time_loan:TimeLoanSkill};
+import { configureEntryArchetypeSkills, EntryFireballSkill, EntrySwordSkill, EntryPoisonNeedleSkill, EntryHeavyHitSkill, EntryIronWallSkill, EntryMovementSkill } from './EntryArchetypeSkills.js';
+
+configureEntryArchetypeSkills();
+
+export const SKILL_HANDLERS={entry_fireball:EntryFireballSkill,entry_sword:EntrySwordSkill,entry_poison_needle:EntryPoisonNeedleSkill,entry_heavy_hit:EntryHeavyHitSkill,entry_iron_wall:EntryIronWallSkill,entry_movement:EntryMovementSkill,bone_boomerang:BoneBoomerangSkill,shadow_fist:ShadowFistSkill,bullet_eater:BulletEaterSkill,parasite_lantern:ParasiteLanternSkill,hanging_blade:HangingBladeSkill,mirror_march:MirrorMarchSkill,judgment_pendulum:JudgmentPendulumSkill,time_loan:TimeLoanSkill};
