de3d730b9dc498f80da6ea708841f4fe239c4eda
ljk5505861 <17605047106@163.com>
2026-06-25 09:47:25 +0800

    Limit runtime handlers to the six entry skills

--- src/skills/handlers/index.js
+++ src/skills/handlers/index.js
@@ -1,13 +1,5 @@
-import BoneBoomerangSkill from './BoneBoomerangSkill.js';
-import ShadowFistSkill from './ShadowFistSkill.js';
-import BulletEaterSkill from './BulletEaterSkill.js';
-import ParasiteLanternSkill from './ParasiteLanternSkill.js';
-import HangingBladeSkill from './HangingBladeSkill.js';
-import MirrorMarchSkill from './MirrorMarchSkill.js';
-import JudgmentPendulumSkill from './JudgmentPendulumSkill.js';
-import TimeLoanSkill from './TimeLoanSkill.js';
 import { configureEntryArchetypeSkills, EntryFireballSkill, EntrySwordSkill, EntryPoisonNeedleSkill, EntryHeavyHitSkill, EntryIronWallSkill, EntryMovementSkill } from './EntryArchetypeSkills.js';
 
 configureEntryArchetypeSkills();
 
-export const SKILL_HANDLERS={entry_fireball:EntryFireballSkill,entry_sword:EntrySwordSkill,entry_poison_needle:EntryPoisonNeedleSkill,entry_heavy_hit:EntryHeavyHitSkill,entry_iron_wall:EntryIronWallSkill,entry_movement:EntryMovementSkill,bone_boomerang:BoneBoomerangSkill,shadow_fist:ShadowFistSkill,bullet_eater:BulletEaterSkill,parasite_lantern:ParasiteLanternSkill,hanging_blade:HangingBladeSkill,mirror_march:MirrorMarchSkill,judgment_pendulum:JudgmentPendulumSkill,time_loan:TimeLoanSkill};
+export const SKILL_HANDLERS={entry_fireball:EntryFireballSkill,entry_sword:EntrySwordSkill,entry_poison_needle:EntryPoisonNeedleSkill,entry_heavy_hit:EntryHeavyHitSkill,entry_iron_wall:EntryIronWallSkill,entry_movement:EntryMovementSkill};
