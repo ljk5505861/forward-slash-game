ec5709c6cd77b4abe35702b310b8129ae18ea5bb
ljk5505861 <17605047106@163.com>
2026-06-29 08:16:02 +0800

    chore: bump version to 0.10.70

--- src/config/version.js
+++ src/config/version.js
@@ -1,2 +1,2 @@
-export const GAME_VERSION = '0.10.69';
-export const GAME_VERSION_LABEL = `v${GAME_VERSION}`;
+export const GAME_VERSION = '0.10.70';
+export const GAME_VERSION_LABEL = 'v' + GAME_VERSION;
